fdc1b0046192d055553cb57fb0ad89fe82604858
Aak101 <77731496+Aak101@users.noreply.github.com>
2021-03-11 17:07:41 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,8 +1,8 @@
 class Box{
 constructor(x,y,width,height){
 var options = {
-    restitution: 0.2,
-    density: 3,
+    restitution: 0.8,
+    density: 1,
     friction: 0.3
 }
 this.body= Bodies.rectangle(x,y,width,height,options);
